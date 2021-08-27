c5888d029c60614fb177c99d93983e50ae50d89b
adriancorro <adriancorrog@gmail.com>
2021-08-27 13:05:07 +0200

    cambios en el server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require("cors");
 const userRouter =  require('./routes/user')
+const path = require('path')
+
 
 // initializing express application
 const app = express();
@@ -8,7 +10,13 @@ const app = express();
 // parse requests of content-type - application/json
 app.use(express.json());
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})
 
 const corsOptions = {
     origin: "/"
