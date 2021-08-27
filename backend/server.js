ed1898b90a8edb93acb443b29cd13a8447be5dde
adriancorro <adriancorrog@gmail.com>
2021-08-27 14:07:06 +0200

    test}

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,21 @@ const app = express();
 // parse requests of content-type - application/json
 app.use(express.json());
 
+let root = path.join(__dirname, '../frontend/build/')
+
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(root))
+
+/* app.use(express.static(path.join(__dirname, '../frontend/build/'))) */
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
+/* app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+}) */
+
+ //this is the routing which will redirect your server url to react build file
+ app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 })
 
 const corsOptions = {
