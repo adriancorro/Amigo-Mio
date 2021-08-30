47ca125b1c04d43ca079ba89c370e074a9d5991a
adriancorro <adriancorrog@gmail.com>
2021-08-30 12:34:37 +0200

    make it better fin

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '../frontend/build/index.html'))  
+  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))  
 }) 
 /* app.use(express.static('./frontend/build' ));  
 app.get('/*',  (req , res ) => {
