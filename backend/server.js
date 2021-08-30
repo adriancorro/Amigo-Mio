089fbfe7af9cf67fafa88aa4534bb4d45ad7a17b
adriancorro <adriancorrog@gmail.com>
2021-08-30 13:22:54 +0200

    make it better fin

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
 app.get('/*', (req, res) => {   
- let  url = path.join(__dirname + '/../frontend/build/index.html', 'index.html');
+ let  url = path.join(__dirname + '/../frontend/build/', 'index.html');
  if (!url.startsWith('/app/')) // since we're on local windows
     url = url.substring(1);
     res.sendFile(url); 
