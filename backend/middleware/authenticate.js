e7b5ea9b0f4de2d83bb27370798c62fcda904579
adriancorro <adriancorrog@gmail.com>
2021-08-31 00:25:56 +0200

    test

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -2,6 +2,25 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const user = require("../routes/user");
 
+const config = {
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT, 
+    jwtSecret: process.env.jwtSecret, 
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+};
+
+
+const pool = new Pool(config);
+
+pool.on('connect', () => {
+  console.log('Connected to the Database');
+});
+
 const authenticate = async (req, res, next) => {
     // console.log(req.header("authorization"))
     let token = await req.header("authorization");
