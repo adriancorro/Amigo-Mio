3157eeeb54abeafb62a3634b2c56887d346b0e8e
adriancorro <adriancorrog@gmail.com>
2021-08-31 14:51:08 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 const corsOptions = {
-  origin: `http://localhost:/${PORT}`
+  origin: `http://localhost:${PORT}`
 };
 
 app.use(cors(corsOptions));  // enable CORS */
