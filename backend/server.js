cdf701082b4b03ca667299a15dffc262c84f84e7
adriancorro <adriancorrog@gmail.com>
2021-08-31 16:04:09 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 const corsOptions = {
-  origin: `https://amigo-mio-open-culture-center.herokuapp.com/app`
+  origin: `https://`
 };
 console.log(`${PORT}  epaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`)
 
@@ -19,13 +19,13 @@ app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, '/../frontend/build/')));
-
+/* 
 app.get('/*', (req, res) => {   
  let  url = path.join(__dirname + '/../frontend/build/', 'index.html');
  if (!url.startsWith('/app/')) // since we're on local windows 
     url = url.substring(1);
     res.sendFile(url); 
-}) 
+})  */
 
 }
 
