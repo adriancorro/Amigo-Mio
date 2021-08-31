5031a24a5d1a677bf29d678e71b36cc006c76b17
adriancorro <adriancorrog@gmail.com>
2021-08-31 14:40:06 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -12,14 +12,14 @@ const corsOptions = {
   origin: "https://amigo-mio-open-culture-center.herokuapp.com/"
 };
 
-app.use(cors(corsOptions));  // enable CORS */
+app.use(cors(corsOptions));  // enable CORS   */
 
 app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, '/../frontend/build/')));
 
-app.get('/*', (req, res) => {   
+app.get('*', (req, res) => {   
  let  url = path.join(__dirname + '/../frontend/build/', 'index.html');
  if (!url.startsWith('/app/')) // since we're on local windows 
     url = url.substring(1);
