f90074dfc2452746115d5e3f1a95f5b6ab71cf7b
adriancorro <adriancorrog@gmail.com>
2021-09-01 05:42:56 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,10 @@ const app = express();
 app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
-   app.use(express.static(path.join(__dirname, '/../frontend/build' , 'index.html')));
+   app.use(express.static(path.join(__dirname, '/../frontend/build')));
 
    app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/../frontend/build'));
   })  
 
 /* 
