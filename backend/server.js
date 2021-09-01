8ab6ab00bb47177027e6e2bd0e3d976fc37b81db
adriancorro <adriancorrog@gmail.com>
2021-09-01 03:34:22 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,14 @@ const app = express();
 app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, '/../frontend/build/app/')));
+  app.use(express.static(path.join(__dirname, '/../frontend/build/')));
 /* 
   if (!url.startsWith('/app/')) // we're on local windows
     url = url.substring(1);
   res.sendFile(url); */
+  app.get('*/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../frontend/build/index.html'));
+  });
 
 }
 
@@ -29,9 +32,7 @@ if(process.env.NODE_ENV === "production"){
  https://amigo-mio-open-culture-center.herokuapp.com/Books, 
  try to make a request on the server type get and this behavior 
  is not wanted */
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../frontend/build/index.html'));
-});
+
 
 // Simple route
 app.get('/', (req, res) => {
