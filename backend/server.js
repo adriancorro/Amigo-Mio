82c843d731b7e047f35caa0a003fe5de4b8a7759
adriancorro <adriancorrog@gmail.com>
2021-09-01 05:14:31 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 if(process.env.NODE_ENV === "production"){
    app.use(express.static(path.join(__dirname, '/../frontend/build')));
 
-  app.get('/*', userRouter, function (req, res) {
+  app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '/../frontend/build', 'index.html'));
   }) 
 
