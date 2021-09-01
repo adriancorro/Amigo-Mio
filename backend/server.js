02b178849b5fc5eacdd5db319416dab03823fc87
adriancorro <adriancorrog@gmail.com>
2021-09-01 06:54:22 +0200

    test

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,7 @@ const app = express();
 app.use(express.json());
 
 if(process.env.NODE_ENV === "production"){
-  // Configure body parsing for AJAX requests
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
     app.use(express.static(path.join(__dirname, '/../frontend/build')));
     app.get('/*', (req, res) => {
       res.sendFile(path.join(__dirname, '/../frontend/build', 'index.html'), err => {
